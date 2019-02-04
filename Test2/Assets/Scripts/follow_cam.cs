813a11c7216ad2059085246c289434f306d1064c
erwan.vivien <erwan.vivien@epita.fr>
2019-02-04 22:39:14 +0100

    Adding script for future HPs

---
+++ Test2/Assets/Scripts/follow_cam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow_cam : MonoBehaviour
+{
+    //public Cube cube;
+    public GameObject obj;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - obj.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = obj.transform.position + offset;
+        transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y + 3, obj.transform.position.z - 2);
+    }
+}
