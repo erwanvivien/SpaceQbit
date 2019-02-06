7344001561ef23f3f8184ac656345f0bd0702b56
erwan.vivien <erwan.vivien@epita.fr>
2019-02-06 22:34:03 +0100

    Changed the position of the Camera

--- SpaceQbit_Unity/Assets/Scripts/Cam_Follow.cs
+++ SpaceQbit_Unity/Assets/Scripts/Cam_Follow.cs
@@ -8,11 +8,13 @@ public class Cam_Follow: MonoBehaviour
 
     //public Cube cube;
     public GameObject obj;
+    private Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y + 3, obj.transform.position.z - 2);
+        //transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y + 3, obj.transform.position.z - 2.75f);
+        offset = transform.position - obj.transform.position;
     }
     
     // Update is called once per frame
@@ -20,7 +22,7 @@ public class Cam_Follow: MonoBehaviour
     {
         Vector3 targetPosition = obj.transform.position;
 
-        targetPosition += new Vector3(0, 3.43f, -2);
+        targetPosition += offset;
         
         transform.position = 
             Vector3.SmoothDamp(transform.position, 
