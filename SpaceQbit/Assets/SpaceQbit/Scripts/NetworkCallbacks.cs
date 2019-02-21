d464388d5ddb8b36ef409437a34d747ab64ccb75
Zacharie <zacharie.constans@epita.fr>
2019-02-21 10:09:20 +0100

    Networking

---
+++ SpaceQbit/Assets/SpaceQbit/Scripts/NetworkCallbacks.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[BoltGlobalBehaviourAttribute]
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string map)
+    {
+        // randomize a position
+        var spawnPosition = new Vector3(Random.Range(-16, 16), 0, Random.Range(-16, 16));
+
+        // instantiate cube
+        BoltNetwork.Instantiate(BoltPrefabs.Perso1, spawnPosition, Quaternion.identity);
+    }
+}
