b56368ced638bea85b12809b5db605ab22442400
erwan.vivien <erwan.vivien@epita.fr>
2019-02-27 11:16:26 +0100

    Save avant bordel

---
+++ SpaceQbit/Assets/Scripts/PotentialKey.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using UnityEngine;
+
+public class PotentialKey : MonoBehaviour
+{
+    public KeyCode key;
+    public Texture2D keyIcon;
+
+    public PotentialKey Get()
+    {
+        return this;
+    }
+}
