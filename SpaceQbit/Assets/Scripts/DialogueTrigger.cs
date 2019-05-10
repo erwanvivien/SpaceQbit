68309e5c89031e2be55a187e877d3639492586e0
Gautier Picard <gautierpicard@rocketmail.com>
2019-05-10 20:23:53 +0200

    timeline + dialogue triggered at door

---
+++ SpaceQbit/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private string[] sentences;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Frame_Perso"))
+            return;
+        DialogueManager.instance.Enqueue(sentences);
+        gameObject.SetActive(false);
+    }
+}
+
